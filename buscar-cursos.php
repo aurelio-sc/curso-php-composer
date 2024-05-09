e4daaa79b66956831768edeeb7860466b0fa4f0c
Aurélio Soares <asc.mat@gmail.com>
2024-05-09 16:07:58 -0300

    adciona psr-4 e dependência de desenvolvimento

---
+++ buscar-cursos.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br']);
+
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$courses = $buscador->buscar('/cursos-online-programacao/php');
+foreach ($courses as $course) {
+    displayMessage($course);
+}
